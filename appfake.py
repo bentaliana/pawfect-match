cc1d947f4a92a4b0be6e6274abfcd11a428c949c
Ben Taliana <ben.taliana.23@um.edu.mt>
2024-11-29 01:29:52 +0100

    Added Pet ID

--- appfake.py
+++ appfake.py
@@ -1,4 +1,13 @@
 from flask import Flask, render_template
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+
+# Firebase setup
+cred = credentials.Certificate("firebase-key.json")  # Replace with the actual file name if different
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 
 app = Flask(__name__)
 
